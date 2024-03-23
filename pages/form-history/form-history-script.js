4c6ba7ee72393e69045bcf3ed7651c8438e0ad8f
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-03-23 11:13:59 -0300

    Add loading local storage in form history page

--- pages/form-history/form-history-script.js
+++ pages/form-history/form-history-script.js
@@ -1,2 +1,12 @@
 
-console.log("Test");
+const FORM_HISTORY_KEY = "formHistory";
+
+const historyContainer = document.querySelector(".history-container");
+
+const formHistoryJSON = localStorage.getItem(FORM_HISTORY_KEY);
+
+if (formHistoryJSON == null) {
+    historyContainer.textContent = "No form history found.";
+} else {
+    historyContainer.textContent = formHistoryJSON;
+}
