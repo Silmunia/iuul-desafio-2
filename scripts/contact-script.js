a1cbda470d3b2f325e4d8f646d7dcda857c8a348
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-03-23 09:15:28 -0300

    Add storing the previous form submission

--- scripts/contact-script.js
+++ scripts/contact-script.js
@@ -1,6 +1,17 @@
 
 let contactForm = document.querySelector("form");
 
-contactForm.addEventListener('submit', (event) => {
-    localStorage.setItem("test", "test");
-})
+contactForm.addEventListener('submit', () => {
+
+    let formNameEntry = document.getElementById("name");
+    let formEmailEntry = document.getElementById("email");
+    let formMessage = document.querySelector("textarea");
+
+    let submissionObject = {
+        "name": formNameEntry.value,
+        "email": formEmailEntry.value,
+        "message": formMessage.value
+    }
+
+    localStorage.setItem("previousFormSubmission", JSON.stringify(submissionObject));
+});
