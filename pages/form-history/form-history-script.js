7abe65ec63e86757f11260938cdb63a9473e07b5
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-03-23 13:22:35 -0300

    Add styling to form history's page entries

--- pages/form-history/form-history-script.js
+++ pages/form-history/form-history-script.js
@@ -1,12 +1,41 @@
 
 const FORM_HISTORY_KEY = "formHistory";
 
-const historyContainer = document.querySelector(".history-container");
+const historyContainer = document.querySelector(".form-history-container");
 
 const formHistoryJSON = localStorage.getItem(FORM_HISTORY_KEY);
 
 if (formHistoryJSON == null) {
-    historyContainer.textContent = "No form history found.";
+
+    let messageElement = document.createElement("h1");
+    messageElement.textContent = "No form history found.";
+
+    historyContainer.appendChild(messageElement);
 } else {
-    historyContainer.textContent = formHistoryJSON;
+
+    const formHistoryArray = JSON.parse(formHistoryJSON);
+
+    for (let i = formHistoryArray.length - 1; i >= 0; i--) {
+
+        let entryNameElement = document.createElement("h1");
+        entryNameElement.textContent = formHistoryArray[i]["name"];
+
+        let entryEmailElement = document.createElement("h2");
+        entryEmailElement.textContent = formHistoryArray[i]["email"];
+
+        let entryIdentificationContainer = document.createElement("div");
+        entryIdentificationContainer.appendChild(entryNameElement);
+        entryIdentificationContainer.appendChild(entryEmailElement);
+
+        let entryMessageElement = document.createElement("p");
+        entryMessageElement.textContent = formHistoryArray[i]["message"];
+
+        let formEntryContainer = document.createElement("div");
+        formEntryContainer.appendChild(entryIdentificationContainer);
+        formEntryContainer.appendChild(entryMessageElement);
+
+        formEntryContainer.classList.add("form-entry");
+
+        historyContainer.appendChild(formEntryContainer);
+    }
 }
